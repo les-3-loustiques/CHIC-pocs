115e2f88d75bbb2d5a398ccfe41f0896f71b36ea
Unknown <buffopi@gmail.com>
2018-03-18 20:16:29 +0100

    first 3d files with bean holder

--- Pierre/CC2640R2/blueTest/Debug/simple_peripheral_dbg.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-simple_peripheral_dbg.obj: ../simple_peripheral_dbg.c
-simple_peripheral_dbg.obj: C:/Program\ Files\ (x86)/ti/ccs730/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h
-simple_peripheral_dbg.obj: C:/Program\ Files\ (x86)/ti/ccs730/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h
-
-../simple_peripheral_dbg.c: 
-C:/Program\ Files\ (x86)/ti/ccs730/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h: 
-C:/Program\ Files\ (x86)/ti/ccs730/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h: 
